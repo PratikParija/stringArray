1e37bb9e422b230ab13377040f14e52bff1e5996
Pratik Parija <pp285@afsconnect1.njit.edu>
2017-09-27 18:07:37 -0400

    Add function to compare strings

--- stringFunctions.php
+++ stringFunctions.php
@@ -50,13 +50,32 @@
 	$emotion='Love';
 	echo str_repeat($emotion, 5);
 
-	echo '<h2>Functions for converting between character and integers<h2>';
+	echo '<h2>Functions for converting between character and integers</h2>';
 
 	$num=7;
 	echo chr($num) . '<br>';
 	
 	$c=P;
 	echo ord($c) . '<br>';
+	
+	echo '<h2>Functions for comparing strings</h2>'
+
+	echo strcmp("Hello", "hello") . '<br>';
+	echo strcmp("hello", "hello") . '<br>';
+	echo strcmp("hello", "Hello") . '<br>';
+	echo strcmp("2hello", "10hello") . '<br>';
+	echo strnatcmp("2hello", "10hello") . '<br>';
+	echo strnatcasecmp("hello2", "hello10") . '<br>';
+	
+	echo '<h2>Functions for converting integers and strings</h2>';
+
+	echo (int)'7' . '<br>';
+
+
+
+
+
+
 
 
 
