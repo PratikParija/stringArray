caae0bc4995b1f20349f53cf4e2dcb5f28be2556
Pratik Parija <pp285@afsconnect1.njit.edu>
2017-09-27 13:39:36 -0400

    added method to search a string for substring

--- stringFunctions.php
+++ stringFunctions.php
@@ -1,6 +1,8 @@
 <<?php
         echo '<h1>All about string functions</h1>';
 
+	echo '<h2>checking for string input</h2>';
+
 	$firstName = '';
 	if (empty(firstName)){
 
@@ -8,12 +10,25 @@
 
 	}
 
+	echo '<h2>Finding the length and substring </h2>';
+
 	$name='Albert Einstein';
 	$length=strlen($name);
 
 	echo $length"<br>";
 	echo "First Name: " . substr($name, 0, 6) . "<br>";
 	echo "Last Name: " . substr($name, -8, 8) . "<br>";
+
+	echo '<h2>Searching for string</h2>';
+	
+	$info="I am work and study<br>";
+	echo strpos($info, 'work') . '<br>';
+	echo strpos($info, 'study') . '<br>';
+
+	echo '<h2>Modifying strings</h2>
+	
+	
+
 
 ?>
 
