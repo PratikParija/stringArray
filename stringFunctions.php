5573322ff6d5891bed30081da3188de5ce7f35d6
Pratik Parija <pp285@afsconnect1.njit.edu>
2017-09-27 09:22:17 -0400

    Add stringFunctions file starting with a check for empty string

---
+++ stringFunctions.php
@@ -0,0 +1,14 @@
+<<?php
+        echo '<h1>All about string functions</h1>';
+
+	$firstName = '';
+	if (empty(firstName)){
+
+	echo 'Please type in your first name<br>';
+
+	}
+
+	$name='Albert Einstein';
+
+?>
+
