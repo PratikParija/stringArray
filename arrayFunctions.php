0e15f4ccd5cecc2a9189a83f4bf2fbbba8a0fc6b
Pratik Parija <pp285@afsconnect1.njit.edu>
2017-09-27 21:46:51 -0400

    Created an array and an empty array and assigned elements to it

---
+++ arrayFunctions.php
@@ -0,0 +1,20 @@
+<?php
+
+	echo '<h1>All About Array Functions</h1>';
+
+	echo '<h2>Indexed Array</h2>';
+
+	$cars=array("Ferrari", "Porsche", "McLaren");
+
+	echo $cars[0].'<br>';
+
+	$cars = array(0);
+	$cars[0]='Ferrari';
+	$cars[1]='Porsche';
+	$cars[2]='McLaren';
+
+	echo $cars[1].'<br>';
+
+
+
+?>
